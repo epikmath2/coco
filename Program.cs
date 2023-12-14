46bdb2cf29b4bc4836d31daa43b1f42f681c6158
epikmath2 <127064943+epikmath2@users.noreply.github.com>
2023-12-14 14:07:29 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program{
+    string[]  vars;
+        void ECHO(string stri){
+        Console.WriteLine(stri);
+    }
+    public void parseCommand(string command, string[] args){
+        switch (command)
+        {
+            case "ECHO":
+                ECHO(args[0]);
+                break;
+            case "STORE"
+            default:
+
+            break;
+        }
+    }
+}
